950220a08d34a6f8f592942322ebde160f2d5086
Dan Brandt <dan.p.brandt@gmail.com>
2024-03-12 01:55:20 -0500

    Portfolio setup. more styling made for portolio and footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../css/footer.css';
 
 function Footer() {
   return (
     <footer>
       <p>© 2024 Dan Brandt</p>
-      <a href="https://github.com">GitHub</a>
-      <a href="https://linkedin.com">LinkedIn</a>
+      <a href="https://github.com/DB-Doo" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <a href="https://www.linkedin.com/in/danpbrandt/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
     </footer>
   );
 }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,20 +1,58 @@
 import React from 'react';
+import '../css/portfolio.css';
+
+import ticketImage from '../assets/ticket.png';
+import cocktailGeneratorImage from '../assets/cocktail.png';
+import textEditorImage from '../assets/text.png';
+import homeworkTrackerImage from '../assets/homework.png';
+import simpleCalendarImage from '../assets/calandar.png';
+import fiveDayWeatherImage from '../assets/weather.png';
 
 const projects = [
   {
     id: 'project1', 
-    title: 'Project 1',
-    imageUrl: '/path-to-image-1.jpg',
-    deployedUrl: 'https://example.com/project-1',
-    githubUrl: 'https://github.com/yourusername/project-1',
+    title: 'Ticket Wizard',
+    imageUrl: ticketImage,
+    deployedUrl: 'https://ticket-wizard-pete-b1e8f95fed38.herokuapp.com/',
+    githubUrl: 'https://github.com/petemaynard/ticket_wizard',
   },
   {
     id: 'project2', 
-    title: 'Project 2',
-    imageUrl: '/path-to-image-2.jpg',
-    deployedUrl: 'https://example.com/project-2',
-    githubUrl: 'https://github.com/yourusername/project-2',
+    title: 'Cocktail Generator',
+    imageUrl: cocktailGeneratorImage,
+    deployedUrl: 'https://ag-bootcamp-umn.github.io/group-project-one/',
+    githubUrl: 'https://github.com/ag-bootcamp-umn/group-project-one',
   },
+  {
+    id: 'project3', 
+    title: 'PWA Text Editor',
+    imageUrl: textEditorImage,
+    deployedUrl: 'https://swa-dan-text-editor-9ae68d39fd46.herokuapp.com/',
+    githubUrl: 'https://github.com/DB-Doo/PWA-text-editor?tab=readme-ov-file#deployment',
+  },
+  {
+    id: 'project4', 
+    title: 'Homework Tracker',
+    imageUrl: homeworkTrackerImage,
+    deployedUrl: 'https://db-doo.github.io/homework-tracker/',
+    githubUrl: 'https://github.com/DB-Doo/homework-tracker?tab=readme-ov-file',
+  },
+  {
+    id: 'project5', 
+    title: 'Simple Calandar',
+    imageUrl: simpleCalendarImage,
+    deployedUrl: 'https://example.com/project-2',
+    githubUrl: 'https://github.com/DB-Doo/simple-calendar',
+  },
+  {
+    id: 'project6', 
+    title: 'Five Day Weather',
+    imageUrl: fiveDayWeatherImage,
+    deployedUrl: 'https://db-doo.github.io/five-day-weather/',
+    githubUrl: 'https://github.com/DB-Doo/five-day-weather',
+  }
+
+
 
 ];
 
